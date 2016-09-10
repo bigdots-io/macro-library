84f163e64c8250321bfd2fa0d5dadbf7250e3525
Roy Kolak <roy.kolak@gmail.com>
2016-09-10 09:42:48 -0500

    implement marquee text

--- macros/text.js
+++ macros/text.js
@@ -12,15 +12,42 @@ class SolidColorMacro extends Macro {
 
   start() {
     var config = this.config;
-
+    var coordinates = [];
     var typeWriter = new TypeWriter({ font: this.config.font});
     typeWriter.text(this.config.text, (item) => {
       this.callbacks.onPixelChange(item.y, item.x, this.config.color);
+      coordinates.push({y: item.y, x: item.x});
     });
+
+    var messageLength = Math.max.apply(Math, coordinates.map(function(coordinate) {
+      return coordinate.x;
+    }));
+
+    if (messageLength > this.dimensions.width) {
+      setTimeout(() => {
+        var offset = 0;
+        this.interval = setInterval(() => {
+          coordinates.forEach((coordinate) => {
+            this.callbacks.onPixelChange(coordinate.y, coordinate.x - offset, '#000000');
+          });
+          coordinates.forEach((coordinate) => {
+            this.callbacks.onPixelChange(coordinate.y, coordinate.x - (offset + 1), this.config.color);
+          });
+
+          if(offset > messageLength) {
+            offset = -(this.dimensions.width);
+          }
+
+          offset += 1;
+        }, this.config.marqueeSpeed);
+      }, this.config.marqueeInitialDelay);
+    }
   }
 
   stop() {
-    // nothing...
+    if (this.config.marquee) {
+      clearInterval(this.interval);
+    }
   }
 }
 
